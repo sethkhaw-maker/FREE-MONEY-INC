3fed8068c6165f132e2365084b9046f394cfc341
YoshePepehands <68284186+YoshePepehands@users.noreply.github.com>
2022-01-26 15:51:16 +0800

    Minigame

--- Project/Assets/Scripts/Minigame/Minigame.cs
+++ Project/Assets/Scripts/Minigame/Minigame.cs
@@ -1,18 +1,35 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 //The real time scrolling minigame controller
 
 public class Minigame : MonoBehaviour
 {
-    //Variables
-    private float minigameWidth;     //Technical stuff, allows free sizing of Minigame width
-    public float hitboxPercentage;  //Percentage of the hitbox will cover on minigame screen
+    //UI Variables
+    public float minigameWidth;     //Technical stuff, allows free sizing of Minigame width
+    [SerializeField] private float hitboxPercentage;  //Percentage of the hitbox will cover on minigame screen
+    public float hitBoxSize;
+    public float hitboxOffset;
+
+    public float finalOffset;
 
     public RectTransform hitboxDisplay;
+    private Slider hitSlider;       //The moving slider
+    public float sliderValue;
 
+    //Gameplay Variables
+    private bool isGaming;
+    public Text scoreText;
+    int score;
     void Start()
     {
+        hitSlider = GetComponentInChildren<Slider>();
         GenerateHitbox();
+    }
+
+    private void Update()
+    {
+        UpdateSlider();
     }
 
     //Generate a hitbox for the minigame
@@ -24,16 +41,45 @@ public class Minigame : MonoBehaviour
         //Set a percentage amount for hitbox
         hitboxPercentage = Random.Range(10f, 40f);
 
+        //hitboxPercentage = 10f; //TEMPTESTING
+
         //Convert value to localised width length
-        float hitBoxSize = hitboxPercentage / 100 * minigameWidth;
+        hitBoxSize = hitboxPercentage / 100 * minigameWidth;
         
         //Set the hitbox width
         hitboxDisplay.sizeDelta = new Vector2(hitBoxSize, 0);
 
         //Get highest possible offset value for hitbox
-        float hitboxOffset = (minigameWidth - hitBoxSize) / 2;
+        hitboxOffset = (minigameWidth - hitBoxSize) / 2;
 
         //Randomize hitbox offset within minigame screen
-        hitboxDisplay.anchoredPosition = new Vector2(Random.Range(-hitboxOffset, hitboxOffset), 0);
+        finalOffset = Random.Range(-hitboxOffset, hitboxOffset);
+        
+        hitboxDisplay.anchoredPosition = new Vector2(finalOffset, 0);
     }
+
+    //Update the slider to move left and right
+    private void UpdateSlider()
+    {
+        sliderValue = Mathf.PingPong(Time.time, 1);
+        hitSlider.value = sliderValue;
+    }
+
+
+    //The button to stop the minigame
+    public void StopMinigame()
+    {
+        isGaming = false;
+
+        float hitboxLeft = (hitboxOffset + finalOffset) / minigameWidth;
+
+        float hitboxRight = (hitboxOffset + finalOffset + hitBoxSize) / minigameWidth;
+        if (sliderValue > hitboxLeft && sliderValue < hitboxRight)
+        {
+            print("WIN!");
+            GenerateHitbox();
+            scoreText.text = score++.ToString();
+        }
+    }
+
 }
