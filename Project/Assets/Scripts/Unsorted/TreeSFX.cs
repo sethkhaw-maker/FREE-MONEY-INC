c5f5fb1fc794667a1954e4ca27bc3ae68bab5dc0
Nanisemo <77495343+Nanisemo@users.noreply.github.com>
2022-03-16 03:50:37 +0800

    trees now have sfx

---
+++ Project/Assets/Scripts/Unsorted/TreeSFX.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeSFX : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        if (hitInfo.CompareTag("Player"))
+        {
+            FindObjectOfType<AudioManager>()?.Play("Tree SFX");
+        }
+    }
+}
