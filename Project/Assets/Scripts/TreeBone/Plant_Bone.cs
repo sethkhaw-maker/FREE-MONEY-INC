2a44c8c6dc79ea0fa521d470ac68d8ddf6b32508
sethkhaw-maker <s.khaw@digipen.edu>
2022-03-15 10:14:27 +0800

    Cool updates

---
+++ Project/Assets/Scripts/TreeBone/Plant_Bone.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant_Bone : MonoBehaviour
+{
+    [Range(0, 20)]
+    public float speed = 0f;
+    [Range(0, 5)]
+    public float RotMultiplier = 1;
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        speed += Time.deltaTime;
+        var Bone = transform.GetChild(0);
+        float Rot = 5;
+        Bone = Bone.GetChild(0);
+        do
+        {
+            Bone.rotation = Quaternion.Euler(0, 0, 90 + Rot * Mathf.Cos(speed) * RotMultiplier);
+            Rot += Rot * 0.5f;
+
+            if (Bone.childCount == 0) break;
+            Bone = Bone.GetChild(0);
+
+        } while (true);
+        
+    }
+}
---
+++ Project/Assets/Scripts/TreeBone/Simple_Bones.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Simple_Bones : MonoBehaviour
+{
+    float m_R = 0;
+    Vector3 m_OriginalScale;
+    Quaternion m_OriginalRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_OriginalScale = transform.localScale;
+        m_OriginalRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_R += Time.deltaTime * 0.6f;
+
+        var S = m_OriginalScale;
+        S.x += (Mathf.Cos(90 + m_R * 5) + Mathf.Cos(m_R * 10)) * 0.005f;
+        S.y += (Mathf.Cos(90 + m_R * 10) + Mathf.Cos(m_R * 10)) * 0.001f;
+
+        transform.rotation = m_OriginalRotation * Quaternion.Euler(0, 0, Mathf.Cos(m_R * 4) * 0.8f);
+        transform.localScale = S;
+    }
+}
