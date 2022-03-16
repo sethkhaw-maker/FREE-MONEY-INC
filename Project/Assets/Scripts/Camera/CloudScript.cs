5335a0bc234de66316dd02526736017905cb64f6
sethkhaw-maker <s.khaw@digipen.edu>
2022-03-16 13:08:00 +0800

    Clouds shadow moving, new scene for intro

---
+++ Project/Assets/Scripts/Camera/CloudScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudScript : MonoBehaviour
+{
+    private float _speed = 2;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * _speed);
+    }
+
+}
--- Project/Assets/Scripts/UI/MainMenu.cs
+++ Project/Assets/Scripts/UI/MainMenu.cs
@@ -17,6 +17,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
     }
+
+    public void IntroCutscene()
+    {
+
+    }
     public void QuitGame()
     {
         Application.Quit();
