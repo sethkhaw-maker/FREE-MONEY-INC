8df4faee589a2f540a3f97f74eb710acb5122577
Constance Lim <constancelim04@gmail.com>
2022-03-26 13:38:53 +0800

    tutorial dialogue completed

--- Project/Assets/Scripts/_Tutorial/TUT_DialogueManager.cs
+++ Project/Assets/Scripts/_Tutorial/TUT_DialogueManager.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class TUT_DialogueManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject tutorialSection;
+    public GameObject dialogue;
+    int dialogueNumber = 1;
 
-    // Update is called once per frame
-    void Update()
+    public void DialogueProgress()
     {
-        
+        //turn off the current text
+        dialogue.SetActive(false);
+        //Get the name for the next dialogue
+        ++dialogueNumber;
+
+        Transform t = tutorialSection.transform.Find("Dialogue_" + dialogueNumber);
+
+        if (t == null)
+        {
+            tutorialSection.SetActive(false);
+        }
+        else
+        {
+            dialogue = tutorialSection.transform.Find("Dialogue_" + dialogueNumber).gameObject;
+            dialogue.SetActive(true);
+            Debug.Log(dialogue);
+        }
     }
 }
