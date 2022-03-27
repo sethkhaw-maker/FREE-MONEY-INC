7e5f2cd9b2043a266d8e10025bc35445862d1569
Nanisemo <77495343+Nanisemo@users.noreply.github.com>
2022-03-27 17:59:48 +0800

    sfx added to tutorial

---
+++ Project/Assets/Scripts/Audio/OceanSFX.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OceanSFX : MonoBehaviour
+{
+    public AudioManager audioManager;
+    bool isInMainMenu;
+    void Start()
+    {
+        isInMainMenu = true;
+        audioManager = GameObject.Find("Audio Manager").GetComponent<AudioManager>();
+        FindObjectOfType<AudioManager>()?.Play("Ocean Waves");
+
+    }
+
+
+    void Update()
+    {
+        if (isInMainMenu)
+        {
+            StartCoroutine(LoopAudio());
+        }
+        else StopCoroutine(LoopAudio());
+    }
+
+    IEnumerator LoopAudio()
+    {
+        yield return new WaitForSeconds(audioManager.sounds[15].clip.length);
+        FindObjectOfType<AudioManager>()?.Play("Ocean Waves");
+    }
+
+    public void Starting()
+    {
+        isInMainMenu = false;
+        audioManager.sounds[15].source.Stop();
+    }
+}
